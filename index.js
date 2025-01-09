ff0580f4de9300aa932c66bdd60f8e8d2edb6f11
Laurent Hirth <laurent.hirth.68@icloud.com>
2025-01-09 19:27:53 +0100

    filter topics to get only device data

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const tableName = "dev-HomeDeviceData";
 const mqttClient = mqtt.connect("mqtt://localhost:1884", {
   username: "mosquitto",
 });
-const topic = "zigbee2mqtt/#";
+const topic = "zigbee2mqtt/home/#";
 
 mqttClient.on("connect", () => {
   console.log("Connected to MQTT broker");
@@ -23,7 +23,7 @@ mqttClient.on("connect", () => {
     if (err) {
       console.error("Failed to subscribe:", err);
     } else {
-      console.log("Subscribed to sensors/test");
+      console.log("Subscribed to:", topic);
     }
   });
 });
@@ -31,19 +31,26 @@ mqttClient.on("connect", () => {
 mqttClient.on("message", async (topic, message) => {
   try {
     const payload = JSON.parse(message.toString());
-    const deviceId = topic.split("/").splice(-1).pop();
+
+    // topic format: zigbee2mqtt/home/ROOM/TYPE/LOCATION
+    // example: zigbee2mqtt/home/chambre/lumiere/lit
+    const [_, __, room, dataType, location] = topic.split("/");
 
     const data = {
-      deviceId,
+      deviceId: `${room}-${dataType}-${location}`,
       timestamp: new Date().toISOString(),
-      dataType: "temperature",
+      dataType,
+      room,
       temperature: payload.temperature,
+      humidity: payload.humidity,
+      energy: payload.energy,
+      state: payload.state,
     };
     const command = new PutCommand({ TableName: tableName, Item: data });
 
     await docClient.send(command);
-    console.log("Data written to DynamoDB:", data);
-  } catch (e) {
+    console.log("Data written to DynamoDB for topic:", topic);
+  } catch (err) {
     console.error("Error processing message:", err);
   }
 });
