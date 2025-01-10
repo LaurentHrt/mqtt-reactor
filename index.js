58ac706adae0bc23e262989b80eda7184a5f5f13
Laurent Hirth <laurent.hirth.68@icloud.com>
2025-01-10 11:17:21 +0100

    add env variables

--- index.js
+++ index.js
@@ -2,28 +2,42 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { fromSSO } from "@aws-sdk/credential-providers";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import mqtt from "mqtt";
+import "dotenv/config";
+
+// Use SSO only for developpement, use Keys for production
+const useSSO = process.env.USE_SSO === "true";
+const ssoProfile = process.env.SSO_PROFILE || "";
 
 // Configure AWS DynamoDB
-const dynamoDBClient = new DynamoDBClient({
+const config = {
   region: "eu-west-3",
-  credentials: fromSSO({ profile: "PowerUserAccess-209479277233" }),
-});
+};
+if (useSSO) {
+  config.credentials = fromSSO({ profile: ssoProfile });
+}
+const dynamoDBClient = new DynamoDBClient(config);
 const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
-const tableName = "dev-HomeDeviceData";
 
+const tableName = process.env.DYNAMODB_TABLENAME || "dev-HomeDeviceData";
+const mqttHost = process.env.MQTT_HOST || "localhost";
+const mqttPort = process.env.MQTT_PORT || 1883;
+const mqttUsername = process.env.MQTT_USERNAME || "mosquitto";
+const topic = process.env.MQTT_TOPIC || "zigbee2mqtt/home/#";
+
+console.log(`Connecting to MQTT broker at ${mqttHost}:${mqttPort}`);
 // Configure MQTT
-const mqttClient = mqtt.connect("mqtt://localhost:1884", {
-  username: "mosquitto",
+const mqttClient = mqtt.connect(`mqtt://${mqttHost}:${mqttPort}`, {
+  username: mqttUsername,
 });
-const topic = "zigbee2mqtt/home/#";
 
 mqttClient.on("connect", () => {
   console.log("Connected to MQTT broker");
+  console.log(`Subscribing to topic ${topic}`);
   mqttClient.subscribe(topic, (err) => {
     if (err) {
       console.error("Failed to subscribe:", err);
     } else {
-      console.log("Subscribed to:", topic);
+      console.log("Subscribed");
     }
   });
 });
