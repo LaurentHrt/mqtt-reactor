b2ef98ac3c880f89951b508abcbe2fbe981e2d70
Laurent Hirth <laurent.hirth.68@icloud.com>
2025-01-10 15:34:43 +0100

    feat: add deduplication behaviour

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ if (useSSO) {
 }
 const dynamoDBClient = new DynamoDBClient(config);
 const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
-
 const tableName = process.env.DYNAMODB_TABLENAME || "dev-HomeDeviceData";
 const mqttHost = process.env.MQTT_HOST || "localhost";
 const mqttPort = process.env.MQTT_PORT || 1883;
 const mqttUsername = process.env.MQTT_USERNAME || "mosquitto";
 const topic = process.env.MQTT_TOPIC || "zigbee2mqtt/home/#";
 
+const deduplicationCache = new Map();
+
 console.log(`Connecting to MQTT broker at ${mqttHost}:${mqttPort}`);
-// Configure MQTT
 const mqttClient = mqtt.connect(`mqtt://${mqttHost}:${mqttPort}`, {
   username: mqttUsername,
 });
@@ -37,19 +37,26 @@ mqttClient.on("connect", () => {
     if (err) {
       console.error("Failed to subscribe:", err);
     } else {
-      console.log("Subscribed");
+      console.log("Subscribed successfully");
     }
   });
 });
 
-mqttClient.on("message", async (topic, message) => {
+mqttClient.on("message", async (topic, messageBuffer) => {
   try {
-    const payload = JSON.parse(message.toString());
+    const message = messageBuffer.toString();
+
+    if (isDuplicate(topic, message)) {
+      console.log("Duplicated message ignored:", topic);
+      return;
+    }
+
+    console.log("Writting to DynamoDB:", topic);
 
     // topic format: zigbee2mqtt/home/ROOM/TYPE/LOCATION
     // example: zigbee2mqtt/home/chambre/lumiere/lit
     const [_, __, room, dataType, location] = topic.split("/");
-
+    const payload = JSON.parse(message);
     const data = {
       deviceId: `${room}-${dataType}-${location}`,
       timestamp: new Date().toISOString(),
@@ -61,10 +68,17 @@ mqttClient.on("message", async (topic, message) => {
       state: payload.state,
     };
     const command = new PutCommand({ TableName: tableName, Item: data });
-
     await docClient.send(command);
-    console.log("Data written to DynamoDB for topic:", topic);
+    console.log("Data written to DynamoDB:", topic);
   } catch (err) {
     console.error("Error processing message:", err);
   }
 });
+
+function isDuplicate(topic, message) {
+  if (deduplicationCache.get(topic) === message) {
+    return true;
+  }
+  deduplicationCache.set(topic, message);
+  return false;
+}
