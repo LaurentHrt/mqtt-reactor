711ecf149c0547a926f96133b71a6917f56c224e
Laurent Hirth <laurent.hirth.68@icloud.com>
2025-01-13 19:56:49 +0100

    refactoring

--- index.js
+++ index.js
@@ -4,119 +4,146 @@ import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import mqtt from "mqtt";
 import "dotenv/config";
 
-// Use SSO only for developpement, use Keys for production
-const useSSO = process.env.USE_SSO === "true";
-const ssoProfile = process.env.SSO_PROFILE || "";
-
-// Configure AWS DynamoDB
+// Configuration
 const config = {
-  region: "eu-west-3",
+  useSSO: process.env.USE_SSO === "true",
+  ssoProfile: process.env.SSO_PROFILE || "",
+  awsRegion: "eu-west-3",
+  tableName: process.env.DYNAMODB_TABLENAME || "dev-HomeDeviceData",
+  mqtt: {
+    host: process.env.MQTT_HOST || "localhost",
+    port: process.env.MQTT_PORT || 1883,
+    username: process.env.MQTT_USERNAME || "mosquitto",
+    topic: process.env.MQTT_TOPIC || "zigbee2mqtt/home/#",
+  },
 };
-if (useSSO) {
-  config.credentials = fromSSO({ profile: ssoProfile });
-}
-const dynamoDBClient = new DynamoDBClient(config);
-const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
-const tableName = process.env.DYNAMODB_TABLENAME || "dev-HomeDeviceData";
-const mqttHost = process.env.MQTT_HOST || "localhost";
-const mqttPort = process.env.MQTT_PORT || 1883;
-const mqttUsername = process.env.MQTT_USERNAME || "mosquitto";
-const topic = process.env.MQTT_TOPIC || "zigbee2mqtt/home/#";
 
+// AWS DynamoDB Setup
+const awsConfig = {
+  region: config.awsRegion,
+};
+if (config.useSSO) {
+  awsConfig.credentials = fromSSO({ profile: config.ssoProfile });
+}
+const dynamoDBClient = new DynamoDBClient(awsConfig);
+const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+// MQTT Setup
+const mqttClient = mqtt.connect(
+  `mqtt://${config.mqtt.host}:${config.mqtt.port}`,
+  {
+    username: config.mqttUsername,
+  },
+);
+
+// Deduplication Cache
 const deduplicationCache = new Map();
 
-console.log(`Connecting to MQTT broker at ${mqttHost}:${mqttPort}`);
-const mqttClient = mqtt.connect(`mqtt://${mqttHost}:${mqttPort}`, {
-  username: mqttUsername,
-});
-
-mqttClient.on("connect", () => {
-  console.log("Connected to MQTT broker");
-  console.log(`Subscribing to topic ${topic}`);
-  mqttClient.subscribe(topic, (err) => {
-    if (err) {
-      console.error("Failed to subscribe:", err);
-    } else {
-      console.log("Subscribed successfully");
-    }
+function initializeMQTT() {
+  mqttClient.on("connect", () => {
+    console.log("Connected to MQTT broker");
+    mqttClient.subscribe(config.mqtt.topic, (err) => {
+      if (err) {
+        console.error("Failed to subscribe to MQTT topic:", err);
+      } else {
+        console.log(`Subscribed to MQTT topic: ${config.mqtt.topic}`);
+      }
+    });
   });
-});
 
-mqttClient.on("message", async (topic, messageBuffer) => {
+  mqttClient.on("message", handleMessage);
+
+  mqttClient.on("error", (err) => {
+    console.error("MQTT error:", err);
+  });
+}
+
+async function handleMessage(topic, messageBuffer) {
   try {
     const message = messageBuffer.toString();
     const parsedPayload = JSON.parse(message);
 
-    const payload = {
-      temperature: parsedPayload.temperature,
-      humidity: parsedPayload.humidity,
-      energy: parsedPayload.energy,
-      state: parsedPayload.state,
-      occupancy: parsedPayload.occupancy,
-      current: parsedPayload.current,
-      power: parsedPayload.power,
-      moving: parsedPayload.moving,
-      position: parsedPayload.position,
-    };
-
-    // topic format: zigbee2mqtt/home/ROOM/TYPE/LOCATION
-    // example: zigbee2mqtt/home/chambre/lumiere/lit
-
-    const commands = [];
-    const [_, __, room, dataType, location] = topic.split("/");
-
-    if (topic === "zigbee2mqtt/home/sdb/lumiere/dual") {
-      commands.push({
-        deviceId: `${room}-${dataType}-${"plafond"}`,
-        ...payload,
-        state: parsedPayload.state_l1,
-      });
-      commands.push({
-        deviceId: `${room}-${dataType}-${"miroir"}`,
-        ...payload,
-        state: parsedPayload.state_l2,
-      });
-    } else if (topic === "zigbee2mqtt/home/cuisine/lumiere/dual") {
-      commands.push({
-        deviceId: `${room}-${dataType}-${"planDeTravail"}`,
-        ...payload,
-        state: parsedPayload.state_l1,
-      });
-      commands.push({
-        deviceId: `${room}-${dataType}-${"plafond"}`,
-        ...payload,
-        state: parsedPayload.state_l2,
-      });
-    } else {
-      commands.push({
-        deviceId: `${room}-${dataType}-${location}`,
-        ...payload,
-      });
-    }
-
+    const commands = prepareCommands(topic, parsedPayload);
     const filteredCommands = commands.filter(
       (command) => !isDuplicate(command),
     );
 
-    if (filteredCommands.length <= 0) {
-      return;
+    if (filteredCommands.length > 0) {
+      await writeToDynamoDB(filteredCommands, topic);
     }
+  } catch (err) {
+    console.error("Error processing MQTT message:", err);
+  }
+}
 
-    console.log("Writting to DynamoDB:", topic);
-    const commandsExecution = filteredCommands.map((command) =>
+function prepareCommands(topic, payload) {
+  const [, , room, dataType, location] = topic.split("/");
+  const commands = [];
+
+  const commonData = {
+    temperature: payload.temperature,
+    humidity: payload.humidity,
+    energy: payload.energy,
+    state: payload.state,
+    occupancy: payload.occupancy,
+    current: payload.current,
+    power: payload.power,
+    moving: payload.moving,
+    position: payload.position,
+  };
+
+  if (topic === "zigbee2mqtt/home/sdb/lumiere/dual") {
+    commands.push(
+      {
+        deviceId: `${room}-${dataType}-plafond`,
+        ...commonData,
+        state: payload.state_l1,
+      },
+      {
+        deviceId: `${room}-${dataType}-miroir`,
+        ...commonData,
+        state: payload.state_l2,
+      },
+    );
+  } else if (topic === "zigbee2mqtt/home/cuisine/lumiere/dual") {
+    commands.push(
+      {
+        deviceId: `${room}-${dataType}-planDeTravail`,
+        ...commonData,
+        state: payload.state_l1,
+      },
+      {
+        deviceId: `${room}-${dataType}-plafond`,
+        ...commonData,
+        state: payload.state_l2,
+      },
+    );
+  } else {
+    commands.push({
+      deviceId: `${room}-${dataType}-${location}`,
+      ...commonData,
+    });
+  }
+
+  return commands;
+}
+
+async function writeToDynamoDB(commands, topic) {
+  try {
+    const executions = commands.map((command) =>
       docClient.send(
         new PutCommand({
-          TableName: tableName,
+          TableName: config.tableName,
           Item: { ...command, timestamp: new Date().toISOString() },
         }),
       ),
     );
-    await Promise.all(commandsExecution);
-    console.log("Data written to DynamoDB:", topic);
+    await Promise.all(executions);
+    console.log("Data written to DynamoDB for topic:", topic);
   } catch (err) {
-    console.error("Error processing message:", err);
+    console.error("Error writing to DynamoDB:", err);
   }
-});
+}
 
 function isDuplicate(command) {
   const value = JSON.stringify(command);
@@ -127,3 +154,6 @@ function isDuplicate(command) {
   deduplicationCache.set(command.deviceId, value);
   return false;
 }
+
+// Start the Service
+initializeMQTT();
