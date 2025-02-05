6cccccfab96ec02a530d2206cda134d38f76ded0
Laurent Hirth <laurent.hirth.68@icloud.com>
2025-02-05 15:37:16 +0100

    feat: better feedback message from pub-service

--- src/pub-service.js
+++ src/pub-service.js
@@ -1,12 +1,16 @@
 import { initializeMQTTClient } from "./mqtt.js";
 import { connect } from "mqtt";
 
+const log = console.log;
+console.log = function () {};
+
 const mqttClient = initializeMQTTClient(connect);
 
 const topic = process.argv[2] || "";
 const payload = process.argv[3] || "";
 
 mqttClient.publishAsync(topic, payload).then(() => {
+  log(`Message successfully published on ${topic} with payload ${payload}`);
   mqttClient.end();
   process.exit(0);
 });
